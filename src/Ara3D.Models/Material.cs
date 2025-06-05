5aec9662cf590143491169af6f267007b51df80f
Christopher Diggins <cdiggins@gmail.com>
2025-06-05 12:46:04 -0400

    Big refactoring of meshe geometry.

--- src/Ara3D.Models/Material.cs
+++ src/Ara3D.Models/Material.cs
@@ -6,5 +6,14 @@ namespace Ara3D.Models
     {
         public static Material Default 
             => new(new Color(0.5f,0.5f,0.5f,0), 0.1f, 0.5f);
+        
+        public Material WithColor(Color color)
+            => new(color, Metallic, Roughness);
+
+        public Material WithMetallic(float metallic)
+            => new(Color, metallic, Roughness);
+
+        public Material WithRoughness(float roughness)
+            => new(Color, Metallic, roughness);
     }
 }
